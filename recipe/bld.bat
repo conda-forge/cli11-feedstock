4be76bf434efc52eefb6b256a81064ece82e36d1
Scott Bailey <sbailey@anaconda.com>
2022-03-01 11:36:55 -0700

    Add author's tests.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,11 +1,27 @@
-mkdir build
-cd build
+:: cmd
 
-cmake ^
-	-G "NMake Makefiles" ^
-	-DCLI11_BUILD_TESTS=OFF ^
-	-DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-	-DCLI11_BUILD_EXAMPLES=OFF ^
-	%SRC_DIR%
+:: Isolate the build.
+mkdir Build
+cd Build
+if errorlevel 1 exit 1
 
-nmake install
+:: Generate the build files.
+cmake .. -G"Ninja" %CMAKE_ARGS% ^
+      -DCLI11_BUILD_TESTS=ON ^
+      -DCLI11_BUILD_EXAMPLES=OFF ^
+      -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+      -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+      -DCMAKE_BUILD_TYPE=Release
+
+
+:: Build and install.
+ninja install
+if errorlevel 1 exit 1
+
+
+:: Perform tests.
+ninja test
+if errorlevel 1 exit 1
+
+:: Error free exit .
+exit 0
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,20 @@
-mkdir build
-cd build
+#!/bin/bash
 
-cmake \
-    -DCMAKE_INSTALL_PREFIX=$PREFIX \
-    -DCLI11_BUILD_TESTS=OFF \
-    -DCLI11_BUILD_EXAMPLES=OFF \
-    -DCMAKE_INSTALL_LIBDIR=lib \
-    ${CMAKE_ARGS} \
-    $SRC_DIR
+# Isolate the build.
+mkdir -p Build
+cd Build || exit 1
 
-make install
+# Generate the build files.
+cmake .. -G"Ninja" ${CMAKE_ARGS} \
+      -DCLI11_BUILD_TESTS=ON \
+      -DCLI11_BUILD_EXAMPLES=OFF \
+      -DCMAKE_PREFIX_PATH=$PREFIX \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DCMAKE_BUILD_TYPE=Release
+
+# Build and install.
+ninja install || exit 1
+
+
+# Perform tests.
+ninja test || exit 1
